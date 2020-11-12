61da84c6472cfe7a5f20d04e6433f4374f64ee3a
ouhouh1936 <ouhouh1936@gamil.com>
2020-11-12 22:18:19 +0900

    poutre

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import mongoose from "mongoose";
 import Lecture from "./models/Lecture";
 import Snack from "./models/Snack";
+import path from "path";
 
 // 192.168.219.191/admin
 
@@ -11,6 +12,7 @@ const PORT = 7000;
 const app = express();
 app.use(morgan(`dev`));
 app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "/assets")));
 
 mongoose.connect(
   `mongodb://4leaf:fourleaf0309@192.168.219.115:27017/admin`,
@@ -39,6 +41,10 @@ app.get("/snack", async (req, res) => {
   const result = await Snack.find({}, {});
 
   console.log(result);
+});
+app.get("/lecture", async (req, res) => {
+  const result = await Lecture.find({}, {});
+  res.render("lecture", { dataList: result });
 });
 app.listen(PORT, () => {
   console.log(`${PORT} server start`);
