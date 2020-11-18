6f393adc429e28f295d59488cb7fb9e13d5d131f
ouhouh1936 <ouhouh1936@gamil.com>
2020-11-18 21:42:38 +0900

    poiuytr

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "/assets")));
 
 mongoose.connect(
-  `mongodb://4leaf:fourleaf0309@192.168.219.115:27017/admin`,
+  `mongodb://4leaf:fourleaf0309@192.168.219.149:27017/admin`,
   {
     dbName: `EDU_1`,
     useNewUrlParser: true,
@@ -46,6 +46,11 @@ app.get("/lecture", async (req, res) => {
   const result = await Lecture.find({}, {});
   res.render("lecture", { dataList: result });
 });
+
+app.get("/board", (req, res) => {
+  res.render("board");
+});
+
 app.listen(PORT, () => {
   console.log(`${PORT} server start`);
 });
